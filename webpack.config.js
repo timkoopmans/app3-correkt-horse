c00ccefa2978bcfbac493b958a57efcac3cf4379
Tim Koopmans <koops@hey.com>
2023-08-29 18:01:13 +1000

    add redirects to work iwht netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,13 @@
-const webpack = require('webpack')
 const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: "production",  // Changed to production
   entry: "./src/index.js",
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js",
     publicPath: '/',
   },
-  devServer: {
-    port: 8003,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    magicHtml: true,
-    historyApiFallback: {
-      index: 'index.html'
-    }
-    // contentBase: path.join(__dirname, 'dist')
-  },
-  plugins: [
-    // fix "process is not defined" error:
-    // (do "npm install process" before running the build)
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify('development')
-    })
-  ],
   module: {
     rules: [{
       test: /\.css$/,
@@ -43,4 +23,4 @@ module.exports = {
       }
     }]
   }
-}
+};
